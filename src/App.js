6eb2fd54c39c9ecabbefb1f8f83b18de2194d7a2
xnarwhals <102434421+xnarwhals@users.noreply.github.com>
2025-01-30 16:05:23 -0800

    Fix Routing Issue
    Day one problem!

--- src/App.js
+++ src/App.js
@@ -7,21 +7,22 @@ import QuickPortfolio from './components/QuickPortfolio';
 import Secret from './components/Secret';
 
 import './App.scss';
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home/ >} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="quick-portfolio" element={<QuickPortfolio />} />
-          <Route path="secret" element={<Secret />} />
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home/ >} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="quick-portfolio" element={<QuickPortfolio />} />
+            <Route path="secret" element={<Secret />} />
+          </Route>
+        </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <DndProvider backend={HTML5Backend}>
-      <BrowserRouter>
+      <BrowserRouter basename="/XavierReact">
         <App />
       </BrowserRouter>
     </DndProvider>
